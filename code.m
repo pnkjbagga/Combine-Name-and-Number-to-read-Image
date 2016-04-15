72bebe35749ed69d927c4c8804ca18c4a0845c6f
pnkjbagga <pnkjbagga@gmail.com>
2016-04-15 23:29:15 +0200

    COMBINE STRING WITH NUMBER

--- code.m
+++ code.m
@@ -1,3 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                      %
+%                    Best Calculator in the World                      %
+%                                                                      %
+%                     By :- Er. Pankaj Bagga                           %
+%                          email : pnkjbagga@live.in                   %
+%                          www.pankajbagga.com                         %
+%                          +91 9779363902                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                      %
+%                     E2MATRIX Research LAB                            %
+%                                                                      %
+%                    Email   : support@e2matrix.com                    %
+%                    Website : www.e2matrix.com                        %
+%                    Blog    : www.e2matrix.com/blog                   %
+%                    Tel     : +91 9041262727                          %
+%                                                                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+msgbox('E2MATRIX Research LAB')
+
 for loop=1:2
 
 Combine = strcat('Img', num2str(loop), '.jpg');
@@ -6,3 +28,5 @@ Image_read=imread(Combine);
 figure(1), imshow(Image_read);
 pause(.5)
 end
+
+msgbox('Download more codes at www.e2matrix.com/blog')
